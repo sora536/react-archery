51a074f4c35d00393a75699428a98868e7b77911
sora <skysksora0327@gmail.com>
2026-06-30 22:21:55 +0900

    fix url

--- src/app/router/router.tsx
+++ src/app/router/router.tsx
@@ -1,38 +1,33 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
 import Layout from "../layout/layout";
 import RecordPage from "../../pages/recordPage";
 import HistoryPage from "../../pages/historyPage";
 import AnalysisPage from "../../pages/analysisPage";
 import NotFoundPage from "../../pages/notFoundPage";
 
-export const router = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: <Layout />,
-      children: [{ index: true, element: <RecordPage /> }],
-    },
-    {
-      path: "/history",
-      element: <Layout />,
-      children: [{ index: true, element: <HistoryPage /> }],
-    },
-    {
-      path: "/analysis",
-      element: <Layout />,
-      children: [{ index: true, element: <AnalysisPage /> }],
-    },
-    {
-      path: "/morePage",
-      element: <Layout />,
-      children: [{ index: true, element: <NotFoundPage /> }],
-    },
-    {
-      path: "*",
-      element: <NotFoundPage />,
-    },
-  ],
+export const router = createHashRouter([
   {
-    basename: "/react-archery",
+    path: "/",
+    element: <Layout />,
+    children: [{ index: true, element: <RecordPage /> }],
   },
-);
+  {
+    path: "/history",
+    element: <Layout />,
+    children: [{ index: true, element: <HistoryPage /> }],
+  },
+  {
+    path: "/analysis",
+    element: <Layout />,
+    children: [{ index: true, element: <AnalysisPage /> }],
+  },
+  {
+    path: "/morePage",
+    element: <Layout />,
+    children: [{ index: true, element: <NotFoundPage /> }],
+  },
+  {
+    path: "*",
+    element: <NotFoundPage />,
+  },
+]);
